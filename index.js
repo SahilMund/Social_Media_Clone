5084f94ba015d5b094ca52904b1dc70f092c0e8f
SahilMund <msahilranjan@gmail.com>
2022-12-24 18:26:28 +0530

    removed API and JWT

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-const passportJWT = require('./config/passport-jwt-strategy');
 
 const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
