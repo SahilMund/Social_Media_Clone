1b960c72c0d310cf55b932ce8466322b53fb8e1a
SahilMund <msahilranjan@gmail.com>
2022-12-24 13:49:56 +0530

    AJAX added

--- config/flash-middleware.js
+++ config/flash-middleware.js
@@ -3,7 +3,5 @@ module.exports.setFlash = function(req, res, next){
         'success': req.flash('success'),
         'error': req.flash('error')
     }
-
-    console.log("flash called");
     next();
 }
